3b3cccaef0642c65b44fcc9f716fea9b2c8ef365
Mark Needham <m.h.needham@gmail.com>
2023-05-13 21:52:40 +0100

    formatting

--- src/test/suite/execution.test.ts
+++ src/test/suite/execution.test.ts
@@ -2,50 +2,50 @@ import * as assert from 'assert';
 import { prepareCode } from '../../execution';  // adjust this import path as needed
 
 suite('Execution Test Suite', () => {
-test('prepareCode function with valid Python code', async () => {
-	try {
-	  const [language, wrappedCode] = await prepareCode("'Hello World'", "python");
-	  assert.strictEqual(language.command, 'python');
-	  assert.strictEqual(wrappedCode, "print('Hello World')");
-	} catch (error) {
-	  if (error instanceof Error) {
-		assert.fail(error);
-	  } else {
-		assert.fail(error as string);
-	  }
-	}
-  });
-  
-
-  test('prepareCode function with commented Python code', async () => {
-	try {
-	  const [language, wrappedCode] = await prepareCode("'Hello' # print('World')", "python");
-	  assert.strictEqual(language.command, 'python');
-	  assert.strictEqual(wrappedCode, "print('Hello')");
-	} catch (error) {
-		if (error instanceof Error) {
-		  assert.fail(error);
-		} else {
-		  assert.fail(error as string);
+	test('prepareCode function with valid Python code', async () => {
+		try {
+			const [language, wrappedCode] = await prepareCode("'Hello World'", "python");
+			assert.strictEqual(language.command, 'python');
+			assert.strictEqual(wrappedCode, "print('Hello World')");
+		} catch (error) {
+			if (error instanceof Error) {
+				assert.fail(error);
+			} else {
+				assert.fail(error as string);
+			}
 		}
-	  }
-  });
-  
-  test('prepareCode function with unsupported language', async () => {
-	try {
-	  await prepareCode("console.log('Hello World')", "unsupported-language");
-	  assert.fail('Expected function to throw, but it did not');
-	} catch (error) {
-	  assert.strictEqual(error, `Language "unsupported-language" is not supported.`);
-	}
-  });
-  
-  test('prepareCode function with empty code', async () => {
-	try {
-	  await prepareCode("", "python");
-	  assert.fail('Expected function to throw, but it did not');
-	} catch (error) {
-	  assert.strictEqual(error, `No code selected`);
-	}
-  });
+	});
+
+
+	test('prepareCode function with commented Python code', async () => {
+		try {
+			const [language, wrappedCode] = await prepareCode("'Hello' # print('World')", "python");
+			assert.strictEqual(language.command, 'python');
+			assert.strictEqual(wrappedCode, "print('Hello')");
+		} catch (error) {
+			if (error instanceof Error) {
+				assert.fail(error);
+			} else {
+				assert.fail(error as string);
+			}
+		}
+	});
+
+	test('prepareCode function with unsupported language', async () => {
+		try {
+			await prepareCode("console.log('Hello World')", "unsupported-language");
+			assert.fail('Expected function to throw, but it did not');
+		} catch (error) {
+			assert.strictEqual(error, `Language "unsupported-language" is not supported.`);
+		}
+	});
+
+	test('prepareCode function with empty code', async () => {
+		try {
+			await prepareCode("", "python");
+			assert.fail('Expected function to throw, but it did not');
+		} catch (error) {
+			assert.strictEqual(error, `No code selected`);
+		}
+	});
 });
